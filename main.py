91188a0e649960eeb3788993cefc906a3830c1d4
Edgar Cajusol <78707201+EdgarCajusol@users.noreply.github.com>
2024-10-18 16:51:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,7 @@
-from flask import Flask, request, jsonify
+from flask import Flask
 
 app = Flask(__name__)
 
-# Ruta para el Webhook de 17Track
-@app.route('/webhook', methods=['POST'])
-def webhook():
-    # Obtén los datos enviados por 17Track
-    data = request.get_json()
-    print(f"Datos recibidos del Webhook: {data}")
-
-    # Aquí puedes procesar los datos y guardarlos en tu base de datos
-    # Por ejemplo, podrías conectar con tu base de datos Railway y actualizar el estado del envío
-
-    return jsonify({'status': 'ok'}), 200
-
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/')
+def home():
+    return "Hello, Flask!"
